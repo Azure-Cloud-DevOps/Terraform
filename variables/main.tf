f65c23e859ee5692a2b31864d60d2a92368f5a30
RajaAadhav <159149197+RajaAadhav@users.noreply.github.com>
2025-05-12 13:16:54 +0530

    Update main.tf

--- variables/main.tf
+++ variables/main.tf
@@ -1,4 +1,9 @@
-resource "azurerm_resource_group" "main" {
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.location
 }
+
