a81afc46530f82dbeadd8d7aff87d56ab70e5a81
RajaAadhav <159149197+RajaAadhav@users.noreply.github.com>
2025-05-12 12:07:43 +0530

    Create local-exec.tf

---
+++ provisioner/local-exec.tf
@@ -0,0 +1,30 @@
+resource "azurerm_linux_virtual_machine" "example" {
+  name                  = "example-vm"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.example.id]
+  size                  = "Standard_B1s"
+  admin_username        = "azureuser"
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+
+  # 🧨 local-exec runs *after* VM is created
+  provisioner "local-exec" {
+    command = "echo 'VM ${self.name} created at ${self.location}' >> vm_log.txt"
+  }
+}
