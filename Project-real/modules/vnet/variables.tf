a2d4d95dcf21ca3d48fcec3b43afd5a73c2304d1
RajaAadhav <159149197+RajaAadhav@users.noreply.github.com>
2025-05-17 00:06:11 +0530

    Update variables.tf

--- Project-real/modules/vnet/variables.tf
+++ Project-real/modules/vnet/variables.tf
@@ -1,32 +1,50 @@
+variable "resource_group" {
+  description = "Name of the resource group"
+  type        = string
+}
+
 variable "location" {
+  description = "Azure region"
   type = string
 }
 
 variable "vnet_name" {
+  description = "Name of the virtual network"
   type = string
 }
 
 variable "address_space" {
+  description = "Address space for the virtual network"
   type = list(string)
 }
 
 variable "subnets" {
+  description = "Map of subnets with name as key and object with address_prefix"
   type = map(object(({
     address_prefix = string
   }))
 }
 
-variable "lb_name" {
-  type = string
-}
-
-variable "route_table_name" {
-  type = string
+variable "nsg_name" {
+  description = "Name of the Network Security Group"
+  type        = string
 }
 
 variable "nic_name" {
+  description = "Name of the Network Interface"
+  type        = string
+}
+
+variable "route_table_name" {
+  description = "Name of the route table"
+  type        = string
+}
+
+variable "lb_name" {
+  description = "Name of the Load Balancer"
   type = string
 }
+
 
              
           
