b65554c632f7756c58917796adccfd1038c42d66
RajaAadhav <159149197+RajaAadhav@users.noreply.github.com>
2025-05-16 22:12:38 +0530

    Update main.tf

--- Project-real/modules/vnet/main.tf
+++ Project-real/modules/vnet/main.tf
@@ -1,4 +1,25 @@
-resource "azurerm_resource_group" "my-rg" {
-  name        = var.resource_group
-  location    = var.location
+# resource "azurerm_resource_group" "my-rg" {
+#  name        = var.resource_group
+#  location    = var.location
+#}
+
+resource "random_id" "rg_name" {
+  byte_length = 8
 }
+
+resource "azurerm_resource_group" "my-rg" {
+  location    = var.location
+  name        = "dev-${random_id.rg_name.hex}-rg"
+}
+
+resource "azurerm_virtual_network" "vent" {
+  name                = var.vnet_name
+  resource_group_name = azurerm_resource_group.my-rg.name
+  address_space       = 10.0.0.0/16 #need to use variable
+  location            = var.location
+}
+
+resource "azurerm_subnet" "subnet" {
+  f
+
+  
