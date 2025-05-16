038a79ee5d5bf0d7c53e7d86614ad1d58ee45c39
RajaAadhav <159149197+RajaAadhav@users.noreply.github.com>
2025-05-17 00:07:08 +0530

    Update output.tf

--- Project-real/modules/vnet/output.tf
+++ Project-real/modules/vnet/output.tf
@@ -1,10 +1,35 @@
-output "dev_vnet_id" {
-  value = azurerm_virtual_network.vnet.vnet_id
+output "resource_group_name" {
+  value = azurerm_resource_group.my-rg.name
+}
+
+output "vnet_name" {
+  value = azurerm_virtual_network.vent.name
 }
 
 output "subnet_ids" {
-  value = {
-    for subnet in azurerm_subnet.subnets : subnet.name => subnet.id
-  }
+  value = { for k, subnet in azurerm_subnet.subnets : k => subnet.id }
 }
 
+output "nsg_id" {
+  value = azurerm_network_security_group.nsg.id
+}
+
+output "nic_id" {
+  value = azurerm_network_interface.nic.id
+}
+
+output "public_ip" {
+  value = azurerm_public_ip.lb_pip.ip_address
+}
+
+output "lb_frontend_ip_config" {
+  value = azurerm_lb.lb.frontend_ip_configuration
+}
+
+output "nat_gateway_id" {
+  value = azurerm_nat_gateway.example.id
+}
+
+output "nat_gateway_public_ip" {
+  value = azurerm_public_ip.natgwpip.ip_address
+}
