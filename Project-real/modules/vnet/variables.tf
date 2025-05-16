1624eb5353b0ce4c3188e7c918ede6954d4954e5
RajaAadhav <159149197+RajaAadhav@users.noreply.github.com>
2025-05-16 22:21:12 +0530

    Create variables.tf

---
+++ Project-real/modules/vnet/variables.tf
@@ -0,0 +1,21 @@
+variable "location" {
+  type = string
+}
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "address_space" {
+  type = list(string)
+}
+
+variable "subnets" {
+  type = map(object(({
+    address_prefix = string
+  }))
+}
+
+             
+             
+          
