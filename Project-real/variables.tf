9d38cd3ea7f85dbe32d9b03c828b36ed8bbffb3a
RajaAadhav <159149197+RajaAadhav@users.noreply.github.com>
2025-05-16 20:00:51 +0530

    Update variables.tf

--- Project-real/variables.tf
+++ Project-real/variables.tf
@@ -1,1 +1,9 @@
+variable "rgname" {
+  type           = string
+  description    = "resource group name"
+}
 
+variable "location" {
+  type          = string
+  description   = "resource location"
+  default       = "canadacentral"
