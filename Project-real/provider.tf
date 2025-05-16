ea592c7a5f8d0445982b2aa32a3e029993f55802
RajaAadhav <159149197+RajaAadhav@users.noreply.github.com>
2025-05-16 20:04:44 +0530

    Update provider.tf

--- Project-real/provider.tf
+++ Project-real/provider.tf
@@ -1,1 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source    = "hasicorp/azurerm"
+      version   =  "~> 4.8.0"
+    }
+  }
+
+  required_version = ">=1.9.0"
+}
+
+provider "azurerm" {
+  features {
+  }
+}
 
