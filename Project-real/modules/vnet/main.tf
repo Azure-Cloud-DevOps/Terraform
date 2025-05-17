9fcb26adbb6731cc46da074f42fc0d2cd7c58dc8
RajaAadhav <159149197+RajaAadhav@users.noreply.github.com>
2025-05-17 06:11:00 +0530

    Update main.tf

--- Project-real/modules/vnet/main.tf
+++ Project-real/modules/vnet/main.tf
@@ -86,3 +86,34 @@ resource "azurerm_subnet_route_table_association" "route_assoc" {
   subnet_id            = each.value.id
   route_table_id       = azurerm_route_table.rt.id
 }
+
+resource "azurerm_public_ip" "natgwpip" {
+  name                = "natgw-publicIP"
+  location            = azurerm_resource_group.my-rg.location
+  resource_group_name = azurerm_resource_group.my-rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  zones               = ["1"]
+}
+
+#add nat gateway to enable outbound traffic from the backend instances
+resource "azurerm_nat_gateway" "example" {
+  name                    = "nat-Gateway"
+  location                = azurerm_resource_group.my-rg.location
+  resource_group_name     = azurerm_resource_group.my-rg.name
+  sku_name                = "Standard"
+  idle_timeout_in_minutes = 10
+  zones                   = ["1"]
+}
+
+resource "azurerm_subnet_nat_gateway_association" "example" {
+  for_each       = azurerm_subnet.subnets
+  subnet_id      = each.value.id
+  nat_gateway_id = azurerm_nat_gateway.example.id
+}
+
+# add nat gateway public ip association
+resource "azurerm_nat_gateway_public_ip_association" "example" {
+  public_ip_address_id = azurerm_public_ip.natgwpip.id
+  nat_gateway_id       = azurerm_nat_gateway.example.id
+}
