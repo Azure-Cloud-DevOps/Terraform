04e1ef540eb71fb7ff84f34aa5ccb6bf2aee1987
RajaAadhav <159149197+RajaAadhav@users.noreply.github.com>
2025-05-17 06:16:15 +0530

    Update variables.tf

--- Project-real/variables.tf
+++ Project-real/variables.tf
@@ -7,3 +7,22 @@ variable "location" {
   type          = string
   description   = "resource location"
   default       = "canadacentral"
+
+#variable "storage_account_name" {
+ # type = string
+#}
+
+# Generate random suffix
+resource "random_string" "storage_suffix" {
+  length  = 6
+  upper   = false
+  lower   = true
+  numeric = true
+  special = false
+}
+
+# Compose storage account name
+locals {
+  storage_account_name = lower("${var.prefix}${random_string.storage_suffix.result}")
+}
+
